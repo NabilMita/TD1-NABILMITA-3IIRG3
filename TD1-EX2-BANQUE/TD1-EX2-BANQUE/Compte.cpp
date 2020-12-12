05337e7efab7615674b4b8bc021182aba7b33e17
NabilMita <30633006+NabilMita@users.noreply.github.com>
2020-12-12 10:53:53 +0100

    Update Compte.cpp

--- TD1-EX2-BANQUE/TD1-EX2-BANQUE/Compte.cpp
+++ TD1-EX2-BANQUE/TD1-EX2-BANQUE/Compte.cpp
@@ -18,17 +18,18 @@ Compte::Compte(unsigned int numC, string nomP, double S)
 	this->solde = S;
 }
 
-bool Compte::retirerArgent(const double &montant) 
+bool Compte::retirerArgent(const double montant) 
 {
 	if (this->solde > montant && this->solde != 0){
 		this->solde -= montant;
 		cout << "\nNous venons de retirer un montant de " << montant << "DH de votre compte!" << endl;
 		return true;
 	}
+	cout << "\nSolde insuffisant!" << endl;
 	return false;
 }
 
-void Compte::deposerArgent(const double &montant)
+void Compte::deposerArgent(const double montant)
 {
 	this->solde += montant;
 	cout << "\nVotre compte a ete accredite d\'un montant de " << montant << "DH!" << endl;
@@ -39,10 +40,12 @@ void Compte::consulterSolde() const
 	cout << "Le solde actuel de votre compte est: " << this->solde << "DH!" << endl;
 }
 
-void Compte::transfererArgent(const double &montant, Compte &destinataire)
+void Compte::transfererArgent(const double montant, Compte &destinataire)
 {
-	destinataire.solde += montant;
-	cout << "\nNous venons d\'effectuer un transfert d\'un montant de " << montant << "DH a votre compte!" << endl;
+	if(this->retirerArgent(montant)){
+		destinataire.deposerArgent(montant);
+		cout << "\nNous venons d\'effectuer un transfert d\'un montant de " << montant << "DH a votre compte!" << endl;
+	}	
 }
 
 void Compte::consulterCompte() const
